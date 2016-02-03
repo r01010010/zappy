22bfa64c01483fe0756d520ed51fc455d702aaf3
r01010010 <r01010010@gmail.com>
2016-02-03 21:12:25 +0100

    KM0

---
+++ index.js
@@ -0,0 +1,20 @@
+var zappy = {
+ objFrom: function(str, last_value){
+  
+  var objs = str.split('.');
+  var r = {};
+  var last = r;
+  
+  for(i=0; i < objs.length; i++) {
+    if(i !== objs.length - 1){
+      last = last[objs[i]] = {};
+    }else{
+      last[objs[i]] = last_value;
+    }
+  }
+  
+  return r;
+ 
+}
+}
+module.exports = zappy;
